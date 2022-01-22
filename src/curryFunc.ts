f4e1d1da6a37fa76963c07e09d6df24a870662fa
mihailryzij0 <mihailryzij0@gmail.com>
2022-01-22 14:33:00 +0300

    add curryFunc.ts

---
+++ src/curryFunc.ts
@@ -0,0 +1,16 @@
+type Callback = (...rest :number[] ) => any
+
+export function curryFunc(func: Callback) {
+  return function curried( ...args:number[]) {
+    if (args.length >= func.length) {
+      return func.apply(curried, args);
+    } 
+      return function( ...args2:number[]) {
+        return curried.apply(curried,args.concat(args2));
+      }
+    
+  };
+
+
+
+}
